abad0a7891c60b543a5055778cd8da6564a5ed00
Ľubomír Jesze <ljesze@uxtweak.com>
2018-07-23 08:51:55 +0200

    feat(broadcast): allow to specify socket ids omitted from broadcast

--- src/Socket/index.js
+++ src/Socket/index.js
@@ -123,10 +123,15 @@ class Socket {
    *
    * @param  {String}   event
    * @param  {Mixed}    data
+   * @param  {Array}    exceptIds
    *
    * @return {void}
    */
-  broadcast (event, data) {
+  broadcast (event, data, exceptIds = [this.id]) {
+    if (!Array.isArray(exceptIds)) {
+      throw GE.InvalidArgumentException.invalidParameter('broadcast expects 3rd parameter to be an array of socket ids', exceptIds)
+    }
+
     const packet = this.connection.makeEventPacket(this.topic, event, data)
 
     /**
@@ -137,8 +142,8 @@ class Socket {
       if (error) {
         return
       }
-      this.channel.broadcastPayload(this.topic, payload, [this.id])
-      ClusterHop.send('broadcast', this.topic, payload)
+      this.channel.broadcastPayload(this.topic, payload, exceptIds)
+      ClusterHop.send('broadcast', this.topic, payload, { ids: exceptIds })
     })
   }
 
@@ -196,7 +201,7 @@ class Socket {
         return
       }
       this.channel.broadcastPayload(this.topic, payload, ids, true)
-      ClusterHop.send('broadcast', this.topic, payload)
+      ClusterHop.send('broadcast', this.topic, payload, { ids, inverse: true })
     })
   }
 
